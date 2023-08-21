75e240a8a187258c6f4b8a8c9d3a4871f3788e83
Ryan Charles <ryancharles040808@gmail.com>
2023-08-21 09:50:38 -0300

    Exercícios até a questão 4 concluídas

---
+++ lista051/exercicio3.py
@@ -0,0 +1,11 @@
+'''
+Desenvolver um programa que apresente os quadrados dos números inteiros de 15 a 200.
+'''
+import math
+
+contador = 15
+
+while (contador <= 200):
+    print(f"O quadro de {contador}: {math.pow(contador,2):.0f}")
+    contador = contador + 1
+
---
+++ lista051/exercicio4.py
@@ -0,0 +1,13 @@
+'''
+Desenvolver um programa que apresente o valor da soma dos cem primeiros números inteiros (1 + 2 + 3 + 4 + ...
++ 97 + 98 + 99 + 100)
+'''
+
+cont = 1
+acumulador = 0
+
+while (cont <= 100):
+    acumulador = acumulador + cont
+    cont = cont + 1
+
+print(f"A soma dos valores de 1 a 100 = {acumulador}")
