5fcedc22be48ed1264aedf3b9ea42ee50bda2e6b
Ryan Charles <ryancharles040808@gmail.com>
2023-08-22 09:59:08 -0300

    Exercícios até a questão 11 concluídas

---
+++ lista051/exercicio13.py
@@ -0,0 +1,20 @@
+'''
+Desenvolver um programa que imprima a tabuada de 3 a 6.
+'''
+
+num3 = 3
+num4 = 4
+num5 = 5
+num6 = 6
+
+cont = 1
+
+while (cont <= 10):
+    elif ( num3 == 3):
+        print(f"{num3} . {cont} = {num3 * cont} ")
+        cont = cont + 1
+    elif ( num4 == 4):
+        print(f"{num4} . {cont} = {num4 * cont} ")
+        cont = cont + 1
+
+
---
+++ lista051/exercicio9.py
@@ -0,0 +1,15 @@
+'''
+Elaborar um programa que apresente no final a soma dos valores pares existentes na faixa de 0 até 500. Utilize
+um laço que efetue a variação de 2 em 2.
+'''
+
+contador = 2
+acumulador = 0
+
+while (contador <= 500):
+    par = contador % 2
+    if ( par == 0):
+        acumulador = acumulador + contador
+        contador = contador + 2
+
+print(f"A soma dos valores de 0 a 500 de 2 em 2 é = {acumulador}")
